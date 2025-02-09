549cfd6d73aaa0066b100f76c20814a678f4b1c0
praaj500 <prashraaj@gmail.com>
2025-02-08 23:33:30 -0800

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 echo "START"
+
+#set variable
 ISTRING="Stringy"
 IINTY=9
 
 echo $ISTRING
 echo $IINTY
 
+#unset variables
 ISTRING=""
 unset IINTY
 
 echo $ISTRING
 echo $IINTY
+
+#Integer increment/decrement
+xyz=0
+echo "xyz: $xyz"
+xyz=$((xyz+1))
+echo "xyz: $xyz"
+xyz=$((xyz-1))
+echo "xyz: $xyz"
 
 echo "END"
